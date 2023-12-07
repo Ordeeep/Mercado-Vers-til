3caa0b24996675e54dc49f49c94a1f0784367395
Pedro Ivan <pedroivan12@live.com>
2023-12-07 11:32:58 -0300

    novo banco

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,21 +1,60 @@
 const express = require('express');
 const axios = require('axios');
+const mongoose = require('mongoose')
+const MONGODB = require('../../passwords.json')
+
 const app = express();
 const port = 3000;
 
+const Produto = mongoose.model('Produto', {
+    name: String,
+    description: String,
+    price: Number,
+    image_url: String
+})
+
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-    try {
-        const { data } = await axios.get("https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=testando");
-        console.log(data);
-        res.send(data);
-    } catch (error) {
-        console.error("Algo deu errado:", error.message);
-        res.status(500).send('Erro interno do servidor');
-    }
+app.get("/", (req, res) => {
+    res.send("Hello World")
 });
 
+
+
+app.get('/produtos', async(req,res) =>{
+    const produtos = await Produto.find()
+    return res.send(produtos)
+})
+
+app.post('/produto', async (req, res) => {
+    const produto = new Produto({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image_url: req.body.image_url
+    })
+    await produto.save()
+    return res.send(produto)
+})
+
+app.put('/produto/:id', async(req,res)=>{
+    const produto = await Produto.findOneAndUpdate({_id:req.params.id}, {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image_url: req.body.image_url
+    }, { new: true})
+
+    return res.send(produto) 
+})
+
+app.delete('/produto/:id', async (req,res) =>{
+    //findByIdAndDelete é um metodo que procura no banco de dados pelo id e já deleta
+    const produto = await Produto.findByIdAndDelete(req.params.id)
+    return res.send(produto)
+})
+
 app.listen(port, () => {
+    mongoose.connect(`mongodb+srv://${MONGODB.DB_USER}:${MONGODB.DB_PASSWORD}@mercadoversatil.xaww1st.mongodb.net/?retryWrites=true&w=majority`)
     console.log('Servidor está rodando na porta: ' + port);
 });
